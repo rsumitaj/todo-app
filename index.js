d192bd5f5cf3ab998e8a8412cf147dd134a8b673
rsumitaj <sumitrajtech@gmail.com>
2021-06-04 20:16:59 +0530

    complete frontend part

--- index.js
+++ index.js
@@ -3,6 +3,38 @@ const port = 8000;
 
 const app = express();
 
+// setup view engine 
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+// middleware
+
+app.use(express.urlencoded());
+
+app.use(express.static('assets'));
+
+// tasks
+
+var tasks = [
+  {
+    task:"todo app",
+    category:"WORK"
+  },
+  {
+    task:"gym",
+    category:"WORKOUT"
+  }
+];
+
+app.get('/',function(req,res){
+  return res.render('home',{
+    title:"TODO App",
+    task_list : tasks
+  })
+});
+
+
 app.listen(port,function(err){
   if(err){
     console.log(`Error : ${err}`);
